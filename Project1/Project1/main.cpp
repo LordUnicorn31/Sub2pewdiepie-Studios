ab608026998e2156eee2c334c3dd23224f253e65
Leukino <miquelqg99@gmail.com>
2019-02-27 12:13:04 +0100

    Creation of folders and project configurations

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,22 @@
+#include "SDL/include/SDL.h"
+#include <iostream>
+
+#pragma comment (lib, "SDL/x86/SDL2main.lib")
+#pragma comment (lib, "SDL/x86/SDL2.lib")
+using namespace std;
+
+int main(int argc, char* argv[]);
+
+int main() {
+	cout << "Hello world" << endl;
+	system("pause");
+	
+	return 0;
+}
+
+int main(int argc, char * argv[])
+{
+	cout << "Hello world" << endl;
+	system("pause");
+	return 0;
+}
