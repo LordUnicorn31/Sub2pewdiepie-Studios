68b1203740269e08de45eb75c11ea3a25486c44f
Leukino <miquelqg99@gmail.com>
2019-06-10 15:19:26 +0200

    Controllers now control each character

--- Street Fighter (hangout 5)/ModuleInput.cpp
+++ Street Fighter (hangout 5)/ModuleInput.cpp
@@ -104,7 +104,7 @@ update_status ModuleInput::PreUpdate()
 					break;
 				}
 				if (SDL_GameControllerGetAttached(controller_player_2) == SDL_FALSE) {
-					controller_player_2 = SDL_GameControllerOpen(i);
+					controller_player_2 = SDL_GameControllerOpen(i+1);
 					break;
 				}
 			}
